bdeebf4a269b1292064546efd04d56d2aa8a6c9d
ChuntaoLu <chuntaolu13@gmail.com>
2014-01-23 00:33:02 -0500

    Setup rspec and guard

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,11 @@ module MyWeb
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.test_framework      :rspec, fixture: true
+      g.fixture_replacement :factory_girl
+    end
+
   end
 end
