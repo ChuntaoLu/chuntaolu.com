a6ecdd9694672c15c334a41f0be9e330ea6bd889
ChuntaoLu <chuntaolu13@gmail.com>
2014-01-25 13:16:57 -0500

    Add markdown support to articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  #refer to redcarpet documentation
+  class HTMLwithPygments < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, :lexer => language,
+                         options: { :lineanchors => true })
+    end
+  end
+
+  def markdown(text)
+    render_options = { hard_wrap: true }
+    renderer = HTMLwithPygments.new(render_options)
+    extensions = { autolink: true,
+                   space_after_headers: true,
+                   fenced_code_blocks: true,
+                   no_intra_emphasis: true,
+                   strikethrough: true,
+                   superscript: true,
+                   footnotes: true }
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+    markdown.render(text).html_safe
+  end
 end
